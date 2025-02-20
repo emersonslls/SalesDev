1465fa64ae7abbe14c89889dfc411ec11d6faa56
Emerson Sales <157742439+emersonslls@users.noreply.github.com>
2025-02-20 10:44:28 -0300

    Link  no projeto My Service

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -100,7 +100,7 @@ function Home() {
               <h2>Landing page, responsiva.</h2>
             </div>
             <div className={`${styles.Btns} ${stylesrespomsive.Btns}`}>
-              <a href="https://pagemanutencao.vercel.app" target="_blank">
+              <a href="https://app-myservice.vercel.app" target="_blank">
                 <button className={`${styles.BtnAcess} ${stylesrespomsive.BtnAcess}`}>
                   Acesse o site
                 </button>
